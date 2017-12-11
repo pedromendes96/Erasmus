f2f11dc235cf69c6f5f569f8dd60d841e8c48a1c
DavidMoniz <DavidMoniz@users.noreply.github.com>
2017-12-11 20:54:49 +0000

    Phase 0

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
